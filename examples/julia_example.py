43fb3520d34ca15c1186f1a99280a6cb48d1a732
ellingsvee <elling.svee@gmail.com>
2025-07-02 12:23:40 +0200

    Minor changes to incorporate with the new questions.

--- examples/julia_example.py
+++ examples/julia_example.py
@@ -1,8 +1,12 @@
 from jutulgpt.graph import graph
+from jutulgpt.utils import load_lines_from_txt
 
 if __name__ == "__main__":
-    question = "Generate Julia code for printing Hello World."
+    questions = load_lines_from_txt("examples/julia_questions.txt")
+    # question = "Generate code for printing Hello World."
     # question = "Write some Julia code that throws an error with an error message when executed."
+    question = questions[2]
+    print("Question:", question)
     result = graph.invoke(
         {"messages": [("user", question)], "iterations": 0, "error": ""}
     )
