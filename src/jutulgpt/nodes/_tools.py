ff51501227478a2c2ddc7213c0f253cabfbea997
ellingsvee <elling.svee@gmail.com>
2025-07-25 17:43:37 +0200

    Remove the read from file as it is moved to the UI

--- src/jutulgpt/nodes/_tools.py
+++ src/jutulgpt/nodes/_tools.py
@@ -3,14 +3,9 @@
 from langgraph.prebuilt import ToolNode
 
 from jutulgpt.configuration import RETRIEVE_FIMBUL
-from jutulgpt.tools import (
-    read_from_file,
-    retrieve_fimbul,
-    retrieve_jutuldarcy,
-    write_to_file,
-)
+from jutulgpt.tools import retrieve_fimbul, retrieve_jutuldarcy, write_to_file
 
-tools = [write_to_file, read_from_file]
+tools = [write_to_file]
 
 # Add the RAG retrievers as tools
 tools.append(retrieve_jutuldarcy)
