c62d0bcf67a8bd4bebdbc8f2f6be77dd13cb55d5
ellingsvee <elling.svee@gmail.com>
2025-07-25 16:03:28 +0200

    Also beeded for retrieving the fimbul

--- src/jutulgpt/tools/retrieve.py
+++ src/jutulgpt/tools/retrieve.py
@@ -5,12 +5,12 @@ from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import InjectedToolArg, tool
 
 import jutulgpt.rag.retrieval as retrieval
-from jutulgpt.utils import get_file_source
 import jutulgpt.rag.split_docs as split_docs
 import jutulgpt.rag.split_examples as split_examples
-from jutulgpt.configuration import BaseConfiguration, INTERACTIVE_ENVIRONMENT
+from jutulgpt.configuration import INTERACTIVE_ENVIRONMENT
 from jutulgpt.human_in_the_loop.response_on_rag import response_on_rag
 from jutulgpt.rag.retriever_specs import RETRIEVER_SPECS
+from jutulgpt.utils import get_file_source
 
 _: bool = load_dotenv(find_dotenv())
 
@@ -36,8 +36,6 @@ def retrieve_jutuldarcy(
         config=config, spec=RETRIEVER_SPECS["jutuldarcy"]["examples"]
     ) as retriever:
         retrieved_examples = retriever.invoke(query)
-
-    configuration = BaseConfiguration.from_runnable_config(config)
 
     if INTERACTIVE_ENVIRONMENT:
         retrieved_docs = response_on_rag(
@@ -71,14 +69,60 @@ def retrieve_jutuldarcy(
     return out
 
 
-# @tool(parse_docstring=True)
-# TODO: RE-Implement
+@tool(parse_docstring=True)
 def retrieve_fimbul(
     query: str, *, config: Annotated[RunnableConfig, InjectedToolArg]
 ) -> str:
     """
-    Temporary not implemented
+    Use this tool to look up any information, usage, or examples from the Fimbul documentation. ALWAYS use this tool before answering any Julia code question about Fimbul.
+
+    Args:
+        query: The string sent into the vectorstore retriever.
+
+    Returns:
+        String containing the formatted output from the retriever
     """
-    raise NotImplementedError(
-        "The Fimbul retrieval tool is not implemented yet. Please check back later."
-    )
+    with retrieval.make_retriever(
+        config=config, spec=RETRIEVER_SPECS["fimbul"]["docs"]
+    ) as retriever:
+        retrieved_docs = retriever.invoke(query)
+    with retrieval.make_retriever(
+        config=config, spec=RETRIEVER_SPECS["fimbul"]["examples"]
+    ) as retriever:
+        retrieved_examples = retriever.invoke(query)
+
+    print("PRINTING THE RETRIEVED DOCUMENTS:")
+    for doc in retrieved_docs:
+        print(f"Source: {doc.metadata['source']}")
+        print(f"- {doc.page_content[:100]}...\n")
+
+    if INTERACTIVE_ENVIRONMENT:
+        retrieved_docs = response_on_rag(
+            retrieved_docs,
+            get_file_source=get_file_source,
+            get_section_path=split_docs.get_section_path,
+            format_doc=split_docs.format_doc,
+            action_name="Modify retrieved documentation",
+        )
+        retrieved_examples = response_on_rag(
+            retrieved_examples,
+            get_file_source=get_file_source,
+            get_section_path=split_examples.get_section_path,
+            format_doc=split_examples.format_doc,
+            action_name="Modify retrieved examples",
+        )
+
+    docs = split_docs.format_docs(retrieved_docs)
+    examples = split_examples.format_examples(retrieved_examples)
+
+    out = f"""
+    # Retrieved from the Fimbul documentation:
+
+    {docs}
+
+    # Retrieved from the Fimbul examples:
+
+    {examples}
+    """
+
+    return out
