9d5eb6f57abbc82b36d557c144d875fb06b9d4a0
ellingsvee <elling.svee@gmail.com>
2025-07-26 12:52:02 +0200

    Human in the loop to double check shortened code
    Maybe unneccessary

--- src/jutulgpt/human_in_the_loop/__init__.py
+++ src/jutulgpt/human_in_the_loop/__init__.py
@@ -1,7 +1,13 @@
+from jutulgpt.human_in_the_loop.check_shortened_code import check_shortened_code
 from jutulgpt.human_in_the_loop.edges.decide_check_code import decide_check_code
 from jutulgpt.human_in_the_loop.nodes.response_on_generated_code import (
     response_on_generated_code,
 )
 from jutulgpt.human_in_the_loop.response_on_rag import response_on_rag
 
-__all__ = ["decide_check_code", "response_on_generated_code", "response_on_rag"]
+__all__ = [
+    "decide_check_code",
+    "response_on_generated_code",
+    "response_on_rag",
+    "check_shortened_code",
+]
---
+++ src/jutulgpt/human_in_the_loop/check_shortened_code.py
@@ -0,0 +1,51 @@
+from langgraph.prebuilt.interrupt import (
+    ActionRequest,
+    HumanInterrupt,
+    HumanInterruptConfig,
+    HumanResponse,
+)
+from langgraph.types import interrupt
+
+from jutulgpt.configuration import INTERACTIVE_ENVIRONMENT
+
+
+def check_shortened_code(imports: str, code: str) -> tuple[str, str]:
+    """
+    The code is shortened before the test run. This interaction allows the user to previev the shoretned code to make sure nothing breaking has happened.
+    """
+    if INTERACTIVE_ENVIRONMENT:
+        interrupt_message = "The code has automatically been shortened for faster runtime and for avoiding potential softlocks. Fix potential errors, or ifnore to run it as is."
+
+        description = interrupt_message
+
+        # Create the human interrupt request
+        request = HumanInterrupt(
+            action_request=ActionRequest(
+                action="Check shortened code",
+                args={"imports": imports, "code": code},
+            ),
+            config=HumanInterruptConfig(
+                allow_ignore=True,
+                allow_accept=True,
+                allow_respond=False,
+                allow_edit=False,
+            ),
+            description=description,
+        )
+
+        # Wait for the user's response
+        human_response: HumanResponse = interrupt([request])[0]
+        response_type = human_response.get("type")
+
+        if response_type == "accept":
+            args_dics = human_response.get("args", {}).get("args", {})
+            imports = args_dics.get("imports", imports)
+            code = args_dics.get("code", code)
+
+        elif human_response.get("type") == "ignore":
+            pass
+        else:
+            raise TypeError(
+                f"Interrupt value of type {type(human_response)} is not supported."
+            )
+    return imports, code
