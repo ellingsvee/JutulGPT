153ab8d4ee983fc757f2d294ff76efacbe42c1c2
ellingsvee <elling.svee@gmail.com>
2025-07-26 12:52:43 +0200

    Change name from FAISS for more generality

--- src/jutulgpt/rag/retriever_specs.py
+++ src/jutulgpt/rag/retriever_specs.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
+from functools import partial
 from typing import Callable
 
 from jutulgpt.configuration import (
+    N_RETRIEVED_DOCS,
+    N_RETRIEVED_EXAMPLES,
     PROJECT_ROOT,
     USE_LOCAL_MODEL,
 )
@@ -16,9 +19,10 @@ class RetrieverSpec:
     collection_name: str
     filetype: str
     split_func: Callable
+    n_retrieved: int
 
 
-faiss_dir_name = "ollama" if USE_LOCAL_MODEL else "openai"
+retriever_dir_name = "ollama" if USE_LOCAL_MODEL else "openai"
 RETRIEVER_SPECS = {
     "jutuldarcy": {
         "docs": RetrieverSpec(
@@ -26,8 +30,8 @@ RETRIEVER_SPECS = {
             persist_path=str(
                 PROJECT_ROOT
                 / "rag"
-                / "faiss_store"
-                / f"faiss_jutuldarcy_docs_{faiss_dir_name}"
+                / "retriever_store"
+                / f"retriever_jutuldarcy_docs_{retriever_dir_name}"
             ),
             cache_path=str(
                 PROJECT_ROOT / "rag" / "loaded_store" / "loaded_jutuldarcy_docs.pkl"
@@ -35,21 +39,26 @@ RETRIEVER_SPECS = {
             collection_name="jutuldarcy_docs",
             filetype="md",
             split_func=split_docs.split_docs,
+            n_retrieved=N_RETRIEVED_DOCS,
         ),
         "examples": RetrieverSpec(
             dir_path=str(PROJECT_ROOT / "rag" / "jutuldarcy" / "examples"),
             persist_path=str(
                 PROJECT_ROOT
                 / "rag"
-                / "faiss_store"
-                / f"faiss_jutuldarcy_examples_{faiss_dir_name}"
+                / "retriever_store"
+                / f"retriever_jutuldarcy_examples_{retriever_dir_name}"
             ),
             cache_path=str(
                 PROJECT_ROOT / "rag" / "loaded_store" / "loaded_jutuldarcy_examples.pkl"
             ),
             collection_name="jutuldarcy_examples",
             filetype="jl",
-            split_func=split_examples.split_examples,
+            split_func=partial(
+                split_examples.split_examples,
+                header_to_split_on=2,  # Split on `# #` and `# ##`
+            ),
+            n_retrieved=N_RETRIEVED_EXAMPLES,
         ),
     },
     "fimbul": {
@@ -58,8 +67,8 @@ RETRIEVER_SPECS = {
             persist_path=str(
                 PROJECT_ROOT
                 / "rag"
-                / "faiss_store"
-                / f"faiss_fimbul_docs_{faiss_dir_name}"
+                / "retriever_store"
+                / f"retriever_fimbul_docs_{retriever_dir_name}"
             ),
             cache_path=str(
                 PROJECT_ROOT / "rag" / "loaded_store" / "loaded_fimbul_docs.pkl"
@@ -67,21 +76,26 @@ RETRIEVER_SPECS = {
             collection_name="fimbul_docs",
             filetype="md",
             split_func=split_docs.split_docs,
+            n_retrieved=N_RETRIEVED_DOCS,
         ),
         "examples": RetrieverSpec(
             dir_path=str(PROJECT_ROOT / "rag" / "fimbul" / "examples"),
             persist_path=str(
                 PROJECT_ROOT
                 / "rag"
-                / "faiss_store"
-                / f"faiss_fimbul_examples_{faiss_dir_name}"
+                / "retriever_store"
+                / f"retriever_fimbul_examples_{retriever_dir_name}"
             ),
             cache_path=str(
                 PROJECT_ROOT / "rag" / "loaded_store" / "loaded_fimbul_examples.pkl"
             ),
             collection_name="fimbul_examples",
             filetype="jl",
-            split_func=split_examples.split_examples,
+            split_func=partial(
+                split_examples.split_examples,
+                header_to_split_on=1,  # Split on `# #`
+            ),
+            n_retrieved=N_RETRIEVED_EXAMPLES,
         ),
     },
 }
