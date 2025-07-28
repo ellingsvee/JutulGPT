b2204d383441b8344efd29085e4d1046abbc7f20
ellingsvee <elling.svee@gmail.com>
2025-07-28 11:37:02 +0200

    Remove the check_shortened_code as I doubt it will be used

--- src/jutulgpt/human_in_the_loop/__init__.py
+++ src/jutulgpt/human_in_the_loop/__init__.py
@@ -1,11 +1,9 @@
-from jutulgpt.human_in_the_loop.check_shortened_code import check_shortened_code
 from jutulgpt.human_in_the_loop.modify_rag_query import modify_rag_query
 from jutulgpt.human_in_the_loop.response_on_check_code import response_on_check_code
 from jutulgpt.human_in_the_loop.response_on_rag import response_on_rag
 
 __all__ = [
     "response_on_rag",
-    "check_shortened_code",
     "modify_rag_query",
     "response_on_check_code",
 ]
