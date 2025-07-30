b6defff357daf3310aac8dd5f1aa1799d1074e8f
ellingsvee <elling.svee@gmail.com>
2025-07-30 10:38:13 +0200

    Move the CLI code to its own folder

---
+++ src/jutulgpt/cli/__init__.py
@@ -0,0 +1,5 @@
+import jutulgpt.cli.cli_utils as utils
+from jutulgpt.cli.cli_utils import print_to_console
+from jutulgpt.cli.cli_colorscheme import colorscheme
+
+__all__ = ["colorscheme", "utils"]
