10ae5330f8662f68e5620379bb08682fc38e3b5f
ellingsvee <elling.svee@gmail.com>
2025-08-03 10:33:25 +0200

    Various updates to the state

--- src/jutulgpt/state.py
+++ src/jutulgpt/state.py
@@ -16,6 +16,48 @@ from pydantic import BaseModel, Field
 from typing_extensions import Annotated, Sequence
 
 
+class CodeBlock(BaseModel):
+    """
+    Container for a code block, split into imports and main code.
+
+    - imports: Any import statements needed for the code to run.
+    - code: The main code body (excluding imports).
+
+    """
+
+    imports: str = Field(default="", description="Code block import statements")
+    code: str = Field(
+        default="", description="Code block not including import statements"
+    )
+
+    def is_empty(self) -> bool:
+        if not self.imports and not self.code:
+            return True
+        return False
+
+    def get_full_code(
+        self, within_julia_context: bool = False, return_empty_if_no_code: bool = False
+    ) -> str:
+        """
+        Returns the full code block, optionally wrapped as a Julia markdown code block.
+        If within_julia_context is True, wraps the code in triple backticks and 'julia' for syntax highlighting.
+        """
+
+        # WARNING: This might be unneccessary
+        if return_empty_if_no_code and not self.imports and not self.code:
+            return ""
+
+        full_code = "```julia" if within_julia_context else ""
+        if self.imports:
+            full_code += "\n" if within_julia_context else ""
+            full_code += f"{self.imports}"
+        if self.code:
+            full_code += "\n" if within_julia_context or self.imports else ""
+            full_code += f"{self.code}"
+        full_code += "\n```" if within_julia_context else ""
+        return full_code
+
+
 @dataclass
 class InputState:
     """
@@ -41,6 +83,8 @@ class State(InputState):
     - iterations: Number of steps/turns taken so far (useful for limiting recursion or debugging).
     - regenerate_code: True if the human has asked the agent to regenerate code before executing it.
     - retrieved_context: Context retrieved from RAG agent, available to subsequent agents.
+    - code_block: The current code block being worked on, including imports and main code.
+    - retrieved_function_definitions: Retrieved function definitions. Used by the code-agent.
     """
 
     is_last_step: IsLastStep = field(default=False)
@@ -49,39 +93,5 @@ class State(InputState):
     iterations: int = field(default=0)
     regenerate_code: bool = field(default=False)
     retrieved_context: str = field(default="")
-
-
-class CodeBlock(BaseModel):
-    """
-    Container for a code block, split into imports and main code.
-
-    - imports: Any import statements needed for the code to run.
-    - code: The main code body (excluding imports).
-
-    """
-
-    imports: str = Field(default="", description="Code block import statements")
-    code: str = Field(
-        default="", description="Code block not including import statements"
-    )
-
-    def get_full_code(
-        self, within_julia_context: bool = False, return_empty_if_no_code: bool = False
-    ) -> str:
-        """
-        Returns the full code block, optionally wrapped as a Julia markdown code block.
-        If within_julia_context is True, wraps the code in triple backticks and 'julia' for syntax highlighting.
-        """
-
-        if return_empty_if_no_code and not self.imports and not self.code:
-            return ""
-
-        full_code = "```julia" if within_julia_context else ""
-        if self.imports:
-            full_code += "\n" if within_julia_context else ""
-            full_code += f"{self.imports}"
-        if self.code:
-            full_code += "\n" if within_julia_context or self.imports else ""
-            full_code += f"{self.code}"
-        full_code += "\n```" if within_julia_context else ""
-        return full_code
+    code_block: CodeBlock = field(default_factory=CodeBlock)
+    retrieved_function_documentation: str = field(default="")
