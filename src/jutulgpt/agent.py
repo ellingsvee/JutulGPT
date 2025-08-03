d2045040a900875e1cca1ab5f027c707dcb7496c
ellingsvee <elling.svee@gmail.com>
2025-08-03 13:41:32 +0200

    Avoid the use of the agent_tools variable

--- src/jutulgpt/agent.py
+++ src/jutulgpt/agent.py
@@ -1,7 +1,19 @@
 from jutulgpt.multi_agent_system.agents.coding_agent import CodingAgent
-from jutulgpt.nodes._tools import agent_tools
 from jutulgpt.prompts import DEFAULT_CODER_PROMPT
+from jutulgpt.tools import (
+    read_from_file_tool,
+    retrieve_fimbul_tool,
+    retrieve_jutuldarcy_tool,
+    write_to_file_tool,
+)
 
 agent = CodingAgent(
-    part_of_multi_agent=False, prompt=DEFAULT_CODER_PROMPT, tools=agent_tools
+    part_of_multi_agent=False,
+    prompt=DEFAULT_CODER_PROMPT,
+    tools=[
+        retrieve_fimbul_tool,
+        retrieve_jutuldarcy_tool,
+        read_from_file_tool,
+        write_to_file_tool,
+    ],
 )
