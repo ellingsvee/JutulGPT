0da5220fcac57105dd1da64b61343fd4c2a2c34c
ellingsvee <elling.svee@gmail.com>
2025-08-04 14:47:50 +0200

    Update some better human in the loops

---
+++ src/jutulgpt/human_in_the_loop/response_on_error.py
@@ -0,0 +1,39 @@
+from langgraph.prebuilt.interrupt import (
+    ActionRequest,
+    HumanInterrupt,
+    HumanInterruptConfig,
+    HumanResponse,
+)
+from langgraph.types import interrupt
+
+
+def response_on_error() -> tuple[bool, str]:
+    # Prepare the human-in-the-loop UI request
+    request = HumanInterrupt(
+        action_request=ActionRequest(
+            action="Code failed. Accept to try to fix it, ignore to skip code fixing, and respond to give extra feedback to the model on what might be wrong.",
+        ),
+        config=HumanInterruptConfig(
+            allow_ignore=True,
+            allow_accept=True,
+            allow_respond=True,
+            allow_edit=False,
+        ),
+    )
+
+    # Wait for the user's response from the UI
+    human_response: HumanResponse = interrupt([request])[0]
+    response_type = human_response.get("type")
+
+    if response_type == "accept":
+        return True, ""
+
+    elif response_type == "respond":
+        # return code_block, False, ""
+        raise NotImplementedError(
+            f"Interrupt value of type {response_type} is yet implemented."
+        )
+    elif response_type == "ignore":
+        return False, ""
+    else:
+        raise TypeError(f"Interrupt value of type {response_type} is not supported.")
