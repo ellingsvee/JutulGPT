b2fdbb73cc05d26221a9f24be36fc345499fbd99
ellingsvee <elling.svee@gmail.com>
2025-08-08 13:18:13 +0200

    Remember to import

--- src/jutulgpt/tools/__init__.py
+++ src/jutulgpt/tools/__init__.py
@@ -1,16 +1,29 @@
 """This package contains the nodes for the react agent."""
 
 from jutulgpt.tools.filesystem import read_from_file_tool, write_to_file_tool
+from jutulgpt.tools.other import (
+    GrepSearchTool,
+    ReadFileTool,
+    SemanticSearchTool,
+    get_working_directory_tool,
+    run_julia_code_tool,
+    run_julia_linter_tool,
+)
 from jutulgpt.tools.retrieve import (
     retrieve_fimbul_tool,
-    retrieve_function_signature_tool,
+    retrieve_function_documentation_tool,
     retrieve_jutuldarcy_tool,
 )
+
+semantic_search_tool = SemanticSearchTool()
+grep_search_tool = GrepSearchTool()
+read_file_tool = ReadFileTool()
+
 
 __all__ = [
     "retrieve_jutuldarcy_tool",
     "retrieve_fimbul_tool",
-    "retrieve_function_signature_tool",
+    "retrieve_function_documentation_tool",
     "read_from_file_tool",
     "write_to_file_tool",
 ]
