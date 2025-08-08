79f17e83c7e60300cf63bd1dc1b30700a70aa2c9
ellingsvee <elling.svee@gmail.com>
2025-08-08 13:14:13 +0200

    Remove the retrieval of the documentation. Intsead focus only on examples.

--- src/jutulgpt/tools/retrieve.py
+++ src/jutulgpt/tools/retrieve.py
@@ -7,8 +7,8 @@ from pydantic import BaseModel, Field
 
 # from jutulgpt import configuration
 import jutulgpt.rag.retrieval as retrieval
-import jutulgpt.rag.split_docs as split_docs
 import jutulgpt.rag.split_examples as split_examples
+from jutulgpt.cli import colorscheme, print_to_console
 from jutulgpt.configuration import BaseConfiguration, cli_mode
 from jutulgpt.julia import get_function_documentation_from_list_of_funcs
 from jutulgpt.rag.retriever_specs import RETRIEVER_SPECS
@@ -24,7 +24,7 @@ def make_retrieve_tool(
     @tool(
         name,
         args_schema=input_cls,
-        description=f"""Use this tool to look up any information, usage, or examples from the {doc_label} documentation. ALWAYS use this tool before answering any Julia code question about {doc_label}.""",
+        description=f"""Use this tool to look up full examples from the {doc_label} documentation. Use this tool when answering any Julia code question about {doc_label}.""",
     )
     def retrieve_tool(
         query: str, config: Annotated[RunnableConfig, InjectedToolArg]
@@ -41,20 +41,17 @@ def make_retrieve_tool(
                 from jutulgpt.human_in_the_loop import modify_rag_query
 
                 query = modify_rag_query(query, doc_label)
+        else:
+            print_to_console(
+                text=f"**Original Query:** `{query}`",
+                title=f"Retrieving from {doc_label}",
+                border_style=colorscheme.message,
+            )
 
         if not query.strip():
             return "The retrieval was skipped by the user. It is not relevant to the current question."
 
-        # Retrieve docs/examples
-        with retrieval.make_retriever(
-            config=config,
-            spec=RETRIEVER_SPECS[doc_key]["docs"],
-            retrieval_params=retrieval.RetrievalParams(
-                search_type=configuration.documents_search_type,
-                search_kwargs=configuration.documents_search_kwargs,
-            ),
-        ) as retriever:
-            retrieved_docs = retriever.invoke(query)
+        # Retrieve examples
         with retrieval.make_retriever(
             config=config,
             spec=RETRIEVER_SPECS[doc_key]["examples"],
@@ -73,14 +70,6 @@ def make_retrieve_tool(
             if cli_mode:
                 from jutulgpt.cli.cli_human_interaction import cli_response_on_rag
 
-                if configuration.human_interaction.retrieved_documents:
-                    retrieved_docs = cli_response_on_rag(
-                        docs=retrieved_docs,
-                        get_file_source=get_file_source,
-                        get_section_path=split_docs.get_section_path,
-                        format_doc=split_docs.format_doc,
-                        action_name=f"Modify retrieved {doc_label} documentation",
-                    )
                 if configuration.human_interaction.retrieved_examples:
                     retrieved_examples = cli_response_on_rag(
                         docs=retrieved_examples,
@@ -95,14 +84,6 @@ def make_retrieve_tool(
             else:
                 from jutulgpt.human_in_the_loop import response_on_rag
 
-                if configuration.human_interaction.retrieved_documents:
-                    retrieved_docs = response_on_rag(
-                        retrieved_docs,
-                        get_file_source=get_file_source,
-                        get_section_path=split_docs.get_section_path,
-                        format_doc=split_docs.format_doc,
-                        action_name=f"Modify retrieved {doc_label} documentation",
-                    )
                 if configuration.human_interaction.retrieved_examples:
                     retrieved_examples = response_on_rag(
                         retrieved_examples,
@@ -112,14 +93,11 @@ def make_retrieve_tool(
                         action_name=f"Modify retrieved {doc_label} examples",
                     )
 
-        docs = split_docs.format_docs(retrieved_docs)
+        # docs = split_docs.format_docs(retrieved_docs)
         examples = split_examples.format_examples(retrieved_examples)
 
         format_str = lambda s: s if s != "" else "(empty)"
         out = f"""
-# Retrieved from {doc_label} documentation
-{format_str(docs)}
-
 # Retrieved from {doc_label} examples
 {format_str(examples)}
 """
@@ -143,7 +121,7 @@ class RetrieveFimbulToolInput(BaseModel):
 
 # Create tools
 retrieve_jutuldarcy_tool = make_retrieve_tool(
-    name="retrieve_jutuldarcy",
+    name="get_relevant_examples",
     doc_key="jutuldarcy",
     doc_label="JutulDarcy",
     input_cls=RetrieveJutulDarcyToolInput,
@@ -158,18 +136,21 @@ retrieve_fimbul_tool = make_retrieve_tool(
 
 
 # Tool for retrieving function documentation
-class RetrieveFunctionSignatureToolInput(BaseModel):
+class RetrieveFunctionDocumentationToolInput(BaseModel):
     function_names: List[str] = Field(
-        description="A list of function names to retrieve the function signature for.",
+        description="A list of function names to retrieve the documentation for.",
     )
 
 
-@tool("retrieve_function_signature", args_schema=RetrieveFunctionSignatureToolInput)
-def retrieve_function_signature_tool(
+@tool(
+    "retrieve_function_documentation",
+    args_schema=RetrieveFunctionDocumentationToolInput,
+)
+def retrieve_function_documentation_tool(
     function_names: List[str],
     config: Annotated[RunnableConfig, InjectedToolArg],
 ) -> str:
-    """Use this tool to retrieve the function signature of a specific function from the JutulDarcy documentation. This is useful for understanding how to use a function, its parameters, and return types."""
+    """Use this tool to retrieve the documentation for spesific Julia functions."""
 
     _, retrieved_signatures = get_function_documentation_from_list_of_funcs(
         func_names=function_names
