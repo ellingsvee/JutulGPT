526898351a52f7ff5466b9c78bac73cc27423b67
ellingsvee <elling.svee@gmail.com>
2025-08-08 17:14:51 +0200

    Remove the current iteration of the Multi-Agent system
    it seems like the single-agent workflow outperforms, so need to do some
    chenges

--- src/jutulgpt/multi_agent_system/__init__.py
+++
@@ -1,3 +0,0 @@
-from jutulgpt.multi_agent_system.multi_agent import multi_agent
-
-__all__ = ["multi_agent"]
--- src/jutulgpt/multi_agent_system/agents/__init__.py
+++
@@ -1,4 +0,0 @@
-from jutulgpt.multi_agent_system.agents.coding_agent import CodingAgent
-from jutulgpt.multi_agent_system.agents.rag_agent import RAGAgent
-
-__all__ = ["RAGAgent", "CodingAgent"]
