3d5d1bd2311c29bcc93221d681df2fc4c3867bec
ellingsvee <elling.svee@gmail.com>
2025-08-13 13:14:52 +0200

    Update the examples to remove old and modified code
    Add the tools

---
+++ examples/tools/run_tool.py
@@ -0,0 +1,24 @@
+"""
+Example of how can invoke a tool for testing how it works.
+
+The tools you can test are:
+execute_terminal_command
+run_julia_code
+run_julia_linter
+get_working_directory
+list_files_in_directory
+read_from_file
+write_to_file
+grep_search
+retrieve_function_documentation
+retrieve_jutuldarcy_examples
+
+We use the grep_search as an example here.
+
+"""
+
+from jutulgpt.tools import grep_search
+
+if __name__ == "__main__":
+    query = "MPI"
+    out = grep_search.invoke({"query": query})
--- examples/usage_of_spesific_functions/run_tool.py
+++
@@ -1,6 +0,0 @@
-from jutulgpt.tools import grep_search
-
-query = "MPI"
-
-if __name__ == "__main__":
-    out = grep_search.invoke({"query": query})
