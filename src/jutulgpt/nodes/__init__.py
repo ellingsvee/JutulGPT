08224d8624998ebf065192e511dae54944bf132e
ellingsvee <elling.svee@gmail.com>
2025-08-14 12:18:29 +0200

    Remove some legacy code

--- src/jutulgpt/nodes/__init__.py
+++ src/jutulgpt/nodes/__init__.py
@@ -1,7 +1,3 @@
 from jutulgpt.nodes.check_code import check_code
-from jutulgpt.nodes.coding_agent.get_relevant_function_documentation import (
-    get_relevant_function_documentation,
-)
-from jutulgpt.nodes.get_user_input import get_user_input
 
-__all__ = ["get_user_input", "check_code", "get_relevant_function_documentation"]
+__all__ = ["check_code"]
